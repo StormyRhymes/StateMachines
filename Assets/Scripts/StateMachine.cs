aa459f636c52e3fa9c5eeac23157d788d4be6f5d
StormyRhymes <mperezc2@email.cpcc.edu>
2024-04-13 15:14:06 -0400

    Add climbing swimming walking and idle plus addition scripts

--- Assets/Scripts/StateMachine.cs
+++ Assets/Scripts/StateMachine.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class StateMachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum State
     {
-        
+        Idle,
+        Walking,
+        Swimming,
+        Climbing
     }
 
-    // Update is called once per frame
+    public State currentState = State.Idle;
+
+    Vector3 lastPosition;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
     void Update()
     {
-        
+        switch (currentState)
+        {
+            case State.Idle: Idle(); break;
+            case State.Walking: Walking(); break;
+            case State.Swimming: Swimming(); break;
+            case State.Climbing: Climbing(); break;
+            default: break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.name == "WaterTrigger")
+        {
+            currentState = State.Swimming;
+        }
+        else if (other.name == "WaterTrigger2")
+        {
+            currentState = State.Swimming;
+        }
+       else if (other.name == "MountainTrigger")
+        {
+            currentState = State.Climbing;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        currentState = State.Walking;
+    }
+
+    void Swimming()
+    {
+        Debug.Log("I am Swimming");
+    }
+
+    void Climbing()
+    {
+        Debug.Log("I am Climbing");
+    }
+
+    void Idle()
+    {
+        Debug.Log("I am Idle");
+        if (lastPosition != transform.position)
+        {
+            currentState = State.Walking;
+        }
+        lastPosition = transform.position;
+    }
+
+    void Walking()
+    {
+        Debug.Log("I am Walking");
+        if (lastPosition != transform.position)
+        {
+            currentState = State.Idle;
+        }
+        lastPosition = transform.position;
+    }
+
+    void ChangeState(State newState)
+    {
+        currentState = newState;
     }
 }
